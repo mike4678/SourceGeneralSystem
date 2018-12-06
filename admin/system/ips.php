8d0d8f87965e277036e6f231af8ffc843bcb5602
夜月琉璃 <42951672+mike4678@users.noreply.github.com>
2018-12-07 07:32:03 +0800

    Update ips.php

--- admin/system/ips.php
+++ admin/system/ips.php
@@ -16,13 +16,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	} else if(!isset($_POST['mode'])) 
 		
 	{
-		
 		$sql = "UPDATE system_setting 
 				SET value = CASE vars 
 				WHEN 'ipfirewall_status' THEN '".$_POST['status']."'
 				END 
 				WHERE vars IN ('ipfirewall_status')";
-
 	} else 
 		
 	{
@@ -40,10 +38,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	} else {
 		
 		echo '<script language="JavaScript">window.alert("参数已更新！")</script>';
-		
-		}		
+	}		
 }
-
 
 //更新变量值
 echo '<script> var stat = '.$dou->Info('ipfirewall_status').'; var mode = '.$dou->Info('ipfirewall_mode').';</script>';
@@ -78,24 +74,22 @@ function Tips(data) //名单提示信息
 	{
 		document.getElementsByName("info")[0].innerHTML='&nbsp;&nbsp;&nbsp;白名单：仅允许特定IP访问&nbsp;';
 		
-	} else 
-	{ 
+	} else { 
+		
 		document.getElementsByName("info")[0].innerHTML='&nbsp;&nbsp;&nbsp;黑名单：不允许特定IP访问&nbsp;';
 	}
 }
 </script>
       <div class="tab-body">
-        <br />
         <div class="tab-panel " id="tab-ips">
-                 <form method="post" class="form-x" action="#">
-                 <input type="hidden" value="tab-ips" id="Tab" name="Tab">
+         	<form method="post" class="form-x" action="#">
+              <input type="hidden" value="tab-ips" id="Tab" name="Tab">
 				<div class="form-group">
                 	<div class="label"><label>系统状态</label></div>
                 	<div class="field">
-                        <div class="button-group button-group-small radio">
+                     <div class="button-group button-group-small radio">
 						<label id='run' class='button'><input name='status' value='0' type='radio'><span class='icon icon-check'></span> 启用</label><label id='stop' class='button'><input name='status' value='1' type='radio'><span class='icon icon-times'></span> 禁用</label>	
-
-          </div>
+					 </div>
                     </div>
                 </div>
 				<div class="form-group">
@@ -103,11 +97,10 @@ function Tips(data) //名单提示信息
                 	<div class="field">
                         <div class="button-group button-group-small radio">
 							<label id='black' class='button'><input name='mode' value='0' onclick='javascript:Tips(1)' type='radio' ><span class='icon icon-check'></span> 白名单</label><label id='block' class='button'><input name='mode' value='1' onclick='javascript:Tips(2)' type='radio' ><span class='icon icon-check'></span> 黑名单</label><div class='label' id='info' name='info' style='width:250px;margin-top:0px;margin-left:-50px;'></div>
-
-          </div>
+         				 </div>
                     </div>
                 </div>
-                            <div class="form-group">
+                <div class="form-group">
                 	<div class="label"><label>IP防火墙黑名单/白名单</label></div>
                 	<div class="field">
                         <div class="button-group button-group-small radio">
@@ -122,22 +115,18 @@ function Tips(data) //名单提示信息
 								} 
 								
 							}
+							echo '<script>javascript:MoniterStatus()</script>';
 							?>
-            </select></div>
+            			</select></div>
                     </div>
                 </div>
                 <div class="form-group" style="margin-top:6px" >
                 	<div class="label"><label>操作</label></div>
                 	<div class="field">
-				<input name="databasebak" type="button" class="button" id="databasebak" href='#' onclick="javascript:art.dialog.open('/admin/system/ipsetting.php?m=add', {title: '新增IP地址', width: 420, height: 320})" value="添加" />&nbsp;&nbsp;<input type="button" class="button" href='#' onclick="javascript:art.dialog.open('system/ipsetting.php?m=edit', {title: '修改IP地址信息', width: 420, height: 320})" value="编辑" />&nbsp;&nbsp;<input type="button" class="button" href='#' onclick="javascript:art.dialog.open('/admin/system/ipsetting.php?m=del', {title: '删除IP地址', width: 420, height: 235})" value="删除" />	
+					<input name="databasebak" type="button" class="button" id="databasebak" href='#' onclick="javascript:art.dialog.open('/admin/system/ipsetting.php?m=add', {title: '新增IP地址', width: 420, height: 320})" value="添加" />&nbsp;&nbsp;<input type="button" class="button" href='#' onclick="javascript:art.dialog.open('system/ipsetting.php?m=edit', {title: '修改IP地址信息', width: 420, height: 320})" value="编辑" />&nbsp;&nbsp;<input type="button" class="button" href='#' onclick="javascript:art.dialog.open('/admin/system/ipsetting.php?m=del', {title: '删除IP地址', width: 420, height: 235})" value="删除" />	
                     </div>
                 </div>
                 <div class="form-button"><button class="button bg-main" type="submit">应用</button></div> 
-         </form>	
-        	
+         	</form>	
         </div>
-        
-        </div>
-      </div>
-    </div>
-<script>MoniterStatus()</script>
+     </div>
